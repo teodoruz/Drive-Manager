ef477b207594ed1f042a0a38ced8bd2a1463ef58
Teodoruz <mativi800@gmail.com>
2024-03-26 20:14:24 -0300

    test

--- src/main/java/com/example/ServiceManager/Models/DTOs/CostCenterDTO.java
+++ src/main/java/com/example/ServiceManager/Models/DTOs/CostCenterDTO.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class SectorDTO {
+public class CostCenterDTO {
     private Long Id;
     private String sectorName;
 }
--- src/main/java/com/example/ServiceManager/Repository/ItemRepository.java
+++ src/main/java/com/example/ServiceManager/Repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.example.ServiceManager.Repository;
 
 import com.example.ServiceManager.Models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
